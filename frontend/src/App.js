40bc8191597f7fe64504d5369fb388d8b99a7c2b
PZDuck <agp2281337@gmail.com>
2020-08-01 15:59:50 -0400

    tweaked LoggedInContext Provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./components/Routes";
 import NavBar from "./components/NavBar";
-import LoggedInContext from "./components/LoggedInContext";
+import { LoggedInContext } from "./components/LoggedInContext";
 import "./styles/App.css";
-import { useLocalStorage } from "./Hooks";
 
-// REVISIT
-import Api from "./JoblyApi";
-import JWTDecode from "jwt-decode";
+//  REVISIT
+// import Api from "./JoblyApi";
+// import JWTDecode from "jwt-decode";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [token, setToken] = useLocalStorage("_token", "");
+  //   const [user, setUser] = useState(null);
+  //   const [loggedIn, setLoggedIn] = useState(false);
+  //   const [token, setToken] = useLocalStorage("_token", "");
+
+  const { login, token } = useContext(LoggedInContext);
 
   // In case a user refreshes the page thus forcing the App component to rerender
   // REVISIT; should be another way of accomplishing the same result
   useEffect(() => {
-    if (token) {
-      setLoggedIn(true);
-
-      async function getUser() {
-        setUser(await Api.getUser(JWTDecode(token).username));
-      }
-
-      getUser();
-    }
+    login();
   }, [token]);
 
   return (
     <div className="App">
-      <LoggedInContext.Provider
-        value={{ user, setUser, loggedIn, setLoggedIn, token, setToken }}
-      >
-        <BrowserRouter>
-          <NavBar />
-          <Routes />
-        </BrowserRouter>
-      </LoggedInContext.Provider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes />
+      </BrowserRouter>
     </div>
   );
 }
